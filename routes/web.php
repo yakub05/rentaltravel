2443ba7ed7a1d0921cdb78b1e43a984a26bf89be
annisaariss <annisa.aristawati-2020@vokasi.unair.ac.id>
2024-05-23 23:32:26 +0700

    Middleware Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,34 +18,41 @@ Route::get('/', function () {
     return view('user/tampil/home');
 })->name('user/tampil/home');
 
-Route::get('/dashboard', function () {
-    return view('admin/dashboard');
-})->name('dashboard');
+Route::get('/login', [AuthController::class,'index'])->name('login');
+Route::post('/login', [AuthController::class,'login'])->name('login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/admin', function () {
-    return view('admin/layouts/admin');
-})->name('layouts/admin');
-
-Route::get('/dataadmin', function () {
-    return view('admin/dataadmin');
-})->name('dataadmin');
-
-Route::get('/tambahdataadmin', function () {
-    return view('admin/tambahdataadmin');
-})->name('tambahdataadmin');
-
-Route::get('/editadmin', function () {
-    return view('admin/editadmin');
-})->name('editadmin');
-
-Route::get('/kelolakonten', function () {
-    return view('admin/kelolakonten');
-})->name('kelolakonten');
-
-Route::get('/tambahkonten', function () {
-    return view('admin/tambahkonten');
-})->name('tambahkonten');
-
-Route::get('/editkonten', function () {
-    return view('admin/editkonten');
-})->name('editkonten');
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/dashboard', function () {
+        return view('admin/dashboard');
+    })->name('dashboard')->middleware('auth');;
+    
+    Route::get('/admin', function () {
+        return view('admin/layouts/admin');
+    })->name('layouts/admin');
+    
+    Route::get('/dataadmin', function () {
+        return view('admin/dataadmin');
+    })->name('dataadmin');
+    
+    Route::get('/tambahdataadmin', function () {
+        return view('admin/tambahdataadmin');
+    })->name('tambahdataadmin');
+    
+    Route::get('/editadmin', function () {
+        return view('admin/editadmin');
+    })->name('editadmin');
+    
+    Route::get('/kelolakonten', function () {
+        return view('admin/kelolakonten');
+    })->name('kelolakonten');
+    
+    Route::get('/tambahkonten', function () {
+        return view('admin/tambahkonten');
+    })->name('tambahkonten');
+    
+    Route::get('/editkonten', function () {
+        return view('admin/editkonten');
+    })->name('editkonten');
+});
