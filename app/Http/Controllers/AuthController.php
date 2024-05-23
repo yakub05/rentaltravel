2bac27dfb578efb2255fc16fe9ff471a24762056
annisaariss <annisa.aristawati-2020@vokasi.unair.ac.id>
2024-05-23 23:32:17 +0700

    Controller & FE Login + Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
@@ -22,15 +25,12 @@ class AuthController extends Controller
         $password = $request->input('password');
 
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            // Login sukses, simpan session dan redirect ke dashboard
             $request->session()->regenerate();
             return redirect()->intended('/dashboard');
         }
         
-        // Login gagal, kembali ke halaman login dengan error
-        return back()->withErrors([
-            'email' => 'EmailW salah.',
-        ])->onlyInput('email');
+        Session::flash('loginError', 'Email atau password salah');
+        return back();
     }
 
     public function logout(Request $request)
--- resources/views/admin/layouts/components/navbar.blade.php
+++ resources/views/admin/layouts/components/navbar.blade.php
@@ -1,21 +1,25 @@
 <!-- Navbar -->
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
+    navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
-      <nav aria-label="breadcrumb">
-        <h6 class="font-weight-bolder mb-0">Dashboard Admin Travel</h6>
-      </nav>
-      <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-        <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+        <nav aria-label="breadcrumb">
+            <h6 class="font-weight-bolder mb-0">Dashboard Admin Travel</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            </div>
+            <ul class="navbar-nav  justify-content-end">
+                <li class="nav-item d-flex align-items-center">
+                    <form action="{{ route('logout') }}" method="GET" class="mb-0">
+                        @csrf
+                        <button type="submit" class="nav-link text-body font-weight-bold px-0 border-0 bg-transparent">
+                            <i class="fa fa-user me-sm-1"></i>
+                            <span class="d-sm-inline d-none">Logout</span>
+                        </button>
+                    </form>
+                </li>
+            </ul>
         </div>
-        <ul class="navbar-nav  justify-content-end">
-          <li class="nav-item d-flex align-items-center">
-            <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-              <i class="fa fa-user me-sm-1"></i>
-              <span class="d-sm-inline d-none">Sign out</span>
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
-  </nav>
-  <!-- End Navbar -->
+</nav>
+<!-- End Navbar -->
