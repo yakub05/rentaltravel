986ebc70103c00b0f4838285d1ae0e5ffce812be
annisaariss <annisa.aristawati-2020@vokasi.unair.ac.id>
2024-05-28 22:53:40 +0700

    update navbar dan footer

--- resources/views/user/layouts/components/navbar.blade.php
+++ resources/views/user/layouts/components/navbar.blade.php
@@ -5,6 +5,7 @@
       <li><a class="nav-link scrollto" href="/travel">Travel</a></li>
       <li><a class="nav-link scrollto" href="/artikel">Artikel</a></li>
       <li><a class="nav-link scrollto" href="/portfolio">Portofolio</a></li>
+      <li><a class="nav-link scrollto" href="/testimoni">Testimoni</a></li>
       <li><a class="login scrollto" href="/login">Login</a></li>
     </ul>
     <i class="bi bi-list mobile-nav-toggle"></i>
