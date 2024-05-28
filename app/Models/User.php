13e4ab58a0d0e6e3938dd4f9a007eb79aef28c59
annisaariss <annisa.aristawati-2020@vokasi.unair.ac.id>
2024-05-28 17:37:17 +0700

    model testimoni

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -32,6 +32,11 @@ class User extends Authenticatable
     public function konten()
     {
         return $this->hasMany(Konten::class, 'id_user');
+    }
+
+    public function testimoni()
+    {
+        return $this->hasMany(Testimoni::class, 'id_user');
     }
 
     /**
